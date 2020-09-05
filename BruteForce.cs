6a662aba6b91f483a49ffe90091959e8e7c177a1
sayaPintar <70132549+sayaPintar@users.noreply.github.com>
2020-09-05 20:33:30 +0700

    Add files via upload

---
+++ BruteForce.cs
@@ -0,0 +1,36 @@
+using System;
+
+
+
+public class BruteForce
+{
+    static public void Main()
+    {
+        
+        Console.WriteLine("BruteForce Tool Beta.." + Environment.NewLine + "Coded:Denis");
+        
+        string Password = "myCztF";
+        
+        
+        
+        
+        string[] WordList = {"Admin","Fuck","Kontol","Lonsa","Fukka","myCztF"};
+        
+      for(int x = 0; x < WordList.Length; x++){
+          
+          
+             Console.WriteLine(WordList[x] + " = " + Password);
+          
+      
+        
+        
+        
+        }
+        
+        
+        
+        
+
+    }
+
+}
