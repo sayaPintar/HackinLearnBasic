ca3715be7f1bf35f827020f763c57f38a2ea6df4
sayaPintar <70132549+sayaPintar@users.noreply.github.com>
2020-09-14 14:18:50 +0700

    Create ChallangeFruitBowlC#.cs

---
+++ ChallangeFruitBowlC#.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            int fruit;
+            fruit = Convert.ToInt32(Console.ReadLine());
+            
+            //Logic Here..
+            
+           int result = fruit/2;
+           
+           int pie = 3; 
+           int resultEnd = hasil / 3;
+           
+           Console.WriteLine(resultEnd);
+           
+           
+            //Thanks
+        }
+    }
+}
